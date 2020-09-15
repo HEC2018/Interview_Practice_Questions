7313c0526d9db7c3059a2689c5039f01d86ae18a
Hector(Xinhai) Wei <44350020+HEC2018@users.noreply.github.com>
2020-09-15 12:04:51 -0400

    Add files via upload

--- leetcode_Sep_challenge_length_of_lastwords.py
+++ leetcode_Sep_challenge_length_of_lastwords.py
@@ -30,3 +30,20 @@ class Solution(object):
         2
         16
         """
+        # We can revise the solution through just running through the last word but not split.
+        """
+        Revised:
+        class Solution(object):
+            def lengthOfLastWord(self, s):
+                """
+                :type s: str
+                :rtype: int
+                """
+                end = len(s) - 1
+                while end > 0 and s[end] == " ":
+                    end -= 1
+                begin = end
+                while begin >= 0 and s[begin] != " ":
+                    begin -= 1
+                return end - begin
+        """
